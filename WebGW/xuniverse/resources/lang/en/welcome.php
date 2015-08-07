957c3c56f9958ecb154ae2cffafaf5ba39ca0bb3
Ma Yingkui <maeikei@gmail.com>
2015-08-07 22:27:31 +0900

    add

---
+++ WebGW/xuniverse/resources/lang/en/welcome.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'ThirtyYearBrainResearch' => 'Thirty years later complete the basic research of brain',
+    'ThirtyYearStrongAI' => 'Three decades later evolved artificial general intelligence',
+    'FortyYearBrainRead' => 'Forty years after read the data brain',
+    'FortyYearBrain2Robot' => 'Forty years later the robot brain data upload',
+    'FiftyYearRobot2Universe' => 'Fifty years after the robot colonial Aerospace',
+    'HundredYearDason' => 'One hundred years later we arrived at Kepler galaxy, build Dyson Sphere',
+    'TwoHundredYearHuman' => 'Two hundred years later arrival livable planet, the re-establishment of biological humanity',
+];
