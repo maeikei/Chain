a958b9e2f58f98ad271df7d7243768aa137cf306
Ma Yingkui <maeikei@gmail.com>
2015-08-08 20:08:39 +0900

    good

---
+++ WebGW/xuniverse/public/js/app.js
@@ -0,0 +1,26 @@
+var SUPPORT_LANG = [
+	'en','cn','ja'
+]
+
+$(function () {
+	$('.language_button').click(function(){
+		//console.log(window.location);
+		var url = window.location.href;
+		var pathArray = window.location.pathname.split( '/' );
+		//console.log(pathArray);
+		var lastName = pathArray[pathArray.length -1];
+		//console.log(lastName);
+		var index = SUPPORT_LANG.indexOf(lastName);
+		//console.log(index);
+		if(-1 == index) {
+			if('' !=lastName) {
+				url += '/';
+			}
+			url += this.value;
+		} else {
+			url = url.replace(lastName, this.value);
+		}
+		//console.log(url);
+		window.location.href = url;
+	});
+});
